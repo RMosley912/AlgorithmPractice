ceb6dbdeeba8636f11facbe8ed0e65cbaab46856
RMosley912 <122495055+RMosley912@users.noreply.github.com>
2024-01-17 15:51:18 -0500

    Update squares-of-a-sorted-array.js

--- 03-squares-of-a-sorted-array/Solved/squares-of-a-sorted-array.js
+++ 03-squares-of-a-sorted-array/Solved/squares-of-a-sorted-array.js
@@ -1,16 +1,1 @@
-// Write code to create a function that accepts an array of integers sorted in ascending (increasing) order and returns a new array containing the squares of each number in ascending ord
 
-
-// Alternate solution (less efficient)
-
-// var sortedSquares = function(arr) {
-//   var squares = arr.map(function(num) {
-//     return num ** 2;
-//   });
-
-//   var sortedSquares = squares.sort(function(a, b) {
-//     return a - b;
-//   });
-
-//   return sortedSquares;
-// };
